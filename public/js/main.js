aa915a9e26691f91f44692e45c8466c8d41ad8a9
Maximilien <maximilienchikri@gmail.com>
2020-12-28 16:15:17 +0100

    fin exercice

---
+++ public/js/main.js
@@ -0,0 +1,20 @@
+// exercice 1
+// let nom = "Chikri"
+// let prenom = "Max"
+// prompt("ville")
+// let ville = prompt("ville")
+// alert(ville)
+// console.log(`${nom} + ${prenom}`);
+// // exercice 2
+// let name1 = "mister"
+// let firstname = "mask"
+// let city = "canard ville"
+// let travel = "gotham"
+// console.log(`le voyage de ${city} vers ${travel} à été enregistré au nom de ${name1} ${firstname}`);
+//exercice 2B
+
+let yourName = prompt("votre nom")
+let yourFirstName = prompt("votre prénom")
+let villeDorigin = prompt("ville d'origine")
+let villeSouhaite = prompt("ville souhaité")
+console.log(`${yourName} ${yourFirstName} ${villeDorigin} ${villeSouhaite}`);
